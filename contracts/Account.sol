f7ff0cdc3ebad02c72a6ea7e18bbf0843cc0a67e
biancabuzea200 <biancabuzea200@gmail.com>
2024-03-15 00:16:04 +0100

    initial commit

---
+++ contracts/Account.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.4;
+
+// modules
+import {LSP0ERC725Account} from "@lukso/lsp0-contracts/contracts/LSP0ERC725Account.sol";
+
+// errors
+import {
+    ERC725Y_DataKeysValuesLengthMismatch
+} from "@erc725/smart-contracts/contracts/errors.sol";
+
+// constants
+import {
+    _LSP3_SUPPORTED_STANDARDS_KEY,
+    _LSP3_SUPPORTED_STANDARDS_VALUE
+} from "@lukso/lsp3-contracts/contracts/LSP3Constants.sol";
+
+/**
+ * @title implementation of a LUKSO's Universal Profile based on LSP3
+ * @author Fabian Vogelsteller <fabian@lukso.network>
+ * @dev Implementation of the ERC725Account + LSP1 universalReceiver
+ */
+contract UniversalProfile is LSP0ERC725Account {
+    /**
+     * @notice Deploying the contract with owner set to: `initialOwner`
+     * @dev Set `initialOwner` as the contract owner and set the `SupportedStandards:LSP3UniversalProfile` data key
+     * in the ERC725Y data key/value store. The `constructor` also allows funding the contract on deployment.
+     *
+     * Emitted Events:
+     * - ValueReceived: when the contract is funded on deployment.
+     *
+     * @param initialOwner the owner of the contract
+     */
+    constructor(address initialOwner) payable LSP0ERC725Account(initialOwner) {
+        // set data key SupportedStandards:LSP3UniversalProfile
+        _setData(
+            _LSP3_SUPPORTED_STANDARDS_KEY,
+            _LSP3_SUPPORTED_STANDARDS_VALUE
+        );
+    }
+
+    event ValueReceived(address indexed sender, uint256 value);
+
+    function setData(
+        bytes32 dataKey,
+        bytes memory dataValue
+    ) public payable override {
+        if (msg.value != 0) {
+            emit ValueReceived(msg.sender, msg.value);
+        }
+        _setData(dataKey, dataValue);
+    }
+
+    function setDataBatch(
+        bytes32[] memory dataKeys,
+        bytes[] memory dataValues
+    ) public payable override {
+        if (msg.value != 0) {
+            emit ValueReceived(msg.sender, msg.value);
+        }
+
+        if (dataKeys.length != dataValues.length) {
+            revert ERC725Y_DataKeysValuesLengthMismatch();
+        }
+
+        for (uint256 i = 0; i < dataKeys.length; ) {
+            _setData(dataKeys[i], dataValues[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,36 @@
+
+import { decodeBase64, toUtf8Bytes } from "ethers";
+import { ethers } from "hardhat";
+
+async function main() {
+  const account = await ethers.deployContract('UniversalProfile', [
+    '0x5E479a3cc6AdC64ddf3129AEaE99A27A5Bfe4bed'
+  ]);
+  await account.waitForDeployment();
+  const accountAddress = await account.getAddress();
+
+  console.log(
+    `deployed to ${accountAddress}`
+  );
+
+
+    
+  let song = "{INSERT_YOUR_FAVOURITE_SONG_HERE}";
+
+
+  const setData = await account.setData(ethers.encodeBytes32String('favourite_song'), ethers.toUtf8Bytes(song));
+  await setData.wait();
+  
+  console.log("set favorite_song to; ", song, "in your UP");
+  
+  const getData = await account.getData(ethers.encodeBytes32String('favourite_song'));
+  
+  console.log('getData result a is: ', ethers.toUtf8String(getData), 'which is your favourite song');
+  return
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
